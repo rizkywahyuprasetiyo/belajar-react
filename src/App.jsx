eaa81410d9047c1f9e98e5d6b36cb8f5ef87bad4
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2023-02-15 01:43:43 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function () {
+    return (
+        <div className={'bg-blue-600 grid place-content-center'}>
+            <h1 className={'text-white text-2xl py-1'}>Hellow World</h1>
+        </div>
+    );
+}
