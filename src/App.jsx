e906ce2e3d7a5db331ba7549ba6661ec80bd26dc
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2023-02-17 14:23:38 +0700

    belajar bongkar props dan dynamic props di component button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons-react';
+import React from 'react';
+
 export default function () {
     return (
-        <div className={'bg-blue-600 grid place-content-center'}>
-            <h1 className={'text-white text-2xl py-1'}>Hellow World</h1>
+        <div className={'bg-slate-900 grid place-content-center min-h-screen'}>
+            <div className={'flex gap-x-2'}>
+                <Button onClick={() => console.log('facebook')} type={'submit'}>
+                    <IconBrandFacebook />
+                    Facebook
+                </Button>
+                <Button onClick={() => console.log('twitter')} type={'reset'}>
+                    <IconBrandTwitter />
+                    Twitter
+                </Button>
+            </div>
         </div>
+    );
+}
+
+function Button(props) {
+    const { children, text } = props;
+    return (
+        <button
+            {...props}
+            className={
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 bg-blue-600 text-white px-4 py-2 rounded'
+            }
+        >
+            {text || children}
+        </button>
     );
 }
