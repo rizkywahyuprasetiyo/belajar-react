1994dc76dd12d6c8e9f0682c2d7eb3703894a20a
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2023-02-17 17:06:10 +0700

    belajar concate value dengan clsx & belajar nulis atribut component dengan cara lain

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import {
     IconBrandFacebook,
     IconBrandTwitter,
     IconBrandGithub,
+    IconBrandChrome,
 } from '@tabler/icons-react';
+import clsx from 'clsx';
 import React from 'react';
 
 export default function () {
+    const type = 'submit';
+    const onClick = () => console.log('on click dengan cara lain');
     return (
         <div className={'bg-gray-200 grid place-content-center min-h-screen'}>
             <div className={'flex gap-x-2'}>
+                <Button {...{ type, onClick }}>
+                    <IconBrandChrome />
+                    More
+                </Button>
                 <Button onClick={() => console.log('facebook')} type={'submit'}>
                     <IconBrandFacebook />
                     Facebook
@@ -39,7 +47,10 @@ function Button(props) {
     return (
         <button
             {...props}
-            className={`${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-2 rounded`}
+            className={clsx(
+                className,
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-2 rounded'
+            )}
         >
             {text || children}
         </button>
