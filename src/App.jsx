3ced66d80381d98a5f7c2e8db603cd60f1642297
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2023-02-19 15:46:29 +0700

    belajar useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react';
 import PlaceContentCenter from './components/PlaceContentCenter';
-import Todo from './components/Todo';
+import Card from './components/Card';
+import { useRef, useState } from 'react';
+import Button from './components/Button';
+import Input from './components/Input';
 
 export default function App() {
+    const handleClick = () => {
+        console.log('hit me');
+    };
+
     return (
         <PlaceContentCenter>
-            <Todo />
+            <Card>
+                <Card.Title>useRef Hooks</Card.Title>
+                <Card.Body>
+                    <div className='mb-4'>
+                        <Input placeholder='Isikan email' type='email' isFocused />
+                    </div>
+                    <div className='mb-4'>
+                        <Input placeholder='Isikan dengan Password' type='password' />
+                    </div>
+                    <Button onClick={handleClick}>Tick</Button>
+                </Card.Body>
+            </Card>
         </PlaceContentCenter>
     );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,22 @@
-export default function Input({ type = 'text', ...props }) {
+import { useEffect, useRef } from 'react';
+
+const Input = ({ isFocused = false, type = 'text', ...props }) => {
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        if (isFocused) {
+            inputRef.current.focus();
+        }
+    }, []);
+
     return (
         <input
+            ref={inputRef}
             className={'transition duration-300 w-full focus:ring focus:ring-blue-200 focus:border-blue-400 border-slate-300 shadow-sm rounded-lg'}
             {...props}
             type={type}
         />
     );
-}
+};
+
+export default Input;
