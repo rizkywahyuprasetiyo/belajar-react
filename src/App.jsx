a1149fa976b5f13f1cb648c17ec20a4158241b29
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2023-02-19 23:12:48 +0700

    wip belajar AJAX menggunakan axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,26 @@
+import { useEffect, useState } from 'react';
 import PlaceContentCenter from './components/PlaceContentCenter';
-import Input from './components/Input';
-import { useState, useEffect } from 'react';
-import Button from './components/Button';
+import Card from './components/Card';
+import axios from 'axios';
 
-export default function App() {
-    const [name, setName] = useState('');
-    const [online, setOnline] = useState(false);
-    const [scrollPosition, setScroolPosition] = useState(window.scroll);
+export default function App(props) {
+    const [user, setUser] = useState([]);
 
     useEffect(() => {
-        console.log('saya selalu di render.');
-    });
+        async function getPostData() {
+            const { data } = await axios('https://jsonplaceholder.typicode.com/users');
+            console.log(data);
+        }
 
-    useEffect(() => {
-        console.log('saya hanya di panggil pada saat render pertama kali.');
+        getPostData();
     }, []);
-
-    useEffect(() => {
-        console.log(`Sekarang saya sedang ${online ? 'online' : 'offline'}`);
-    }, [online]);
-
-    function updateScrollPosition() {
-        const windowScrolling = window.scrollY;
-        console.log(`Window scroll position: ${windowScrolling}`);
-        setScroolPosition(windowScrolling);
-    }
-
-    function onKeyDown(e) {
-        console.log(e.code);
-    }
-
-    useEffect(() => {
-        // window.addEventListener('scroll', updateScrollPosition);
-        window.addEventListener('keydown', onKeyDown);
-        return () => {
-            window.removeEventListener('keydown', onKeyDown);
-        };
-    });
 
     return (
         <PlaceContentCenter>
-            <div className='h-[4000px]'>
-                <Input value={name} onChange={(e) => setName(e.target.value)} />
-                <Button onClick={() => setOnline(true)}>Set Online</Button>
-            </div>
+            <Card>
+                <Card.Title>Data Users</Card.Title>
+                <Card.Body>Hai</Card.Body>
+            </Card>
         </PlaceContentCenter>
     );
 }
