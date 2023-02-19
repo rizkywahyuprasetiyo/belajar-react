2ccb3e8ebc8e8aa269d32739aeabb8386dc294fb
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2023-02-19 22:41:06 +0700

    belajar useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
 import PlaceContentCenter from './components/PlaceContentCenter';
-import Card from './components/Card';
-import { useRef, useState } from 'react';
-import Button from './components/Button';
 import Input from './components/Input';
+import { useState, useEffect } from 'react';
+import Button from './components/Button';
 
 export default function App() {
-    const handleClick = () => {
-        console.log('hit me');
-    };
+    const [name, setName] = useState('');
+    const [online, setOnline] = useState(false);
+    const [scrollPosition, setScroolPosition] = useState(window.scroll);
+
+    useEffect(() => {
+        console.log('saya selalu di render.');
+    });
+
+    useEffect(() => {
+        console.log('saya hanya di panggil pada saat render pertama kali.');
+    }, []);
+
+    useEffect(() => {
+        console.log(`Sekarang saya sedang ${online ? 'online' : 'offline'}`);
+    }, [online]);
+
+    function updateScrollPosition() {
+        const windowScrolling = window.scrollY;
+        console.log(`Window scroll position: ${windowScrolling}`);
+        setScroolPosition(windowScrolling);
+    }
+
+    function onKeyDown(e) {
+        console.log(e.code);
+    }
+
+    useEffect(() => {
+        // window.addEventListener('scroll', updateScrollPosition);
+        window.addEventListener('keydown', onKeyDown);
+        return () => {
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    });
 
     return (
         <PlaceContentCenter>
-            <Card>
-                <Card.Title>useRef Hooks</Card.Title>
-                <Card.Body>
-                    <div className='mb-4'>
-                        <Input placeholder='Isikan email' type='email' isFocused />
-                    </div>
-                    <div className='mb-4'>
-                        <Input placeholder='Isikan dengan Password' type='password' />
-                    </div>
-                    <Button onClick={handleClick}>Tick</Button>
-                </Card.Body>
-            </Card>
+            <div className='h-[4000px]'>
+                <Input value={name} onChange={(e) => setName(e.target.value)} />
+                <Button onClick={() => setOnline(true)}>Set Online</Button>
+            </div>
         </PlaceContentCenter>
     );
 }
